3d68c68e6e15f2c58fb21115c13fdca72b5faab2
moz <moz@sfc.wide.ad.jp>
2022-05-25 22:53:58 +0900

    add: fisst

---
+++ backend/main.py
@@ -0,0 +1,47 @@
+import psutil
+import serial
+import time
+
+stacks = []
+STACK_SIZE = 5
+SERIAL_CODE = "COM3"
+
+previous_value = 0
+is_increasing = 123
+
+ser = serial.Serial(SERIAL_CODE, 115200, timeout = 0.1)
+time.sleep(2)
+
+
+def get_single_value(previous_value, current_value):
+    code = current_value < previous_value
+    margin = previous_value * 0.05
+    if  previous_value-margin <= current_value <= previous_value+1:
+        return 0
+    else:
+        return 1 if code else 2
+
+def main():
+    global stacks, STACK_SIZE, previous_value, is_increasing
+    while True:
+        percent_cpu_usage = psutil.cpu_percent(interval=1)
+        if len(stacks) < STACK_SIZE:
+            stacks.append(percent_cpu_usage)
+        else:
+            for i in range(STACK_SIZE):
+                if i < STACK_SIZE-1:
+                    stacks[i] = stacks[i+1]
+            stacks[STACK_SIZE-1] = percent_cpu_usage
+        current_value = sum(stacks) / STACK_SIZE
+        is_increasing = get_single_value(current_value, previous_value)
+        previous_value = sum(stacks) / STACK_SIZE
+        print("signal: {0}, usage: {1}, adj_usage: {2}".format(
+            is_increasing, 
+            percent_cpu_usage,
+            current_value)
+        )
+        print(" received -> {0}".format(ser.readline().decode('utf-8')))
+        ser.write("{}:{}".format(is_increasing, int(current_value)).encode("utf-8"))
+
+if __name__ == "__main__":
+    main()
---
+++ strandtest/strandtest.ino
@@ -0,0 +1,41 @@
+
+
+#include <Adafruit_NeoPixel.h>
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
+
+#define LED_PIN 6
+#define LED_COUNT 4
+
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+
+void setup() {
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
+
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+  Serial.begin(115200);     // シリアル通信を初期化する。通信速度は9600bps
+}
+
+
+
+void loop() {
+  if (Serial.available() > 0){
+    String data = Serial.readString();
+    int arg = data.substring(0,1).toInt();
+    int percentage = data.substring(2).toInt();
+    Serial.println(percentage + ":"+ (String)(percentage / 100.0 * 255.0));
+    for (int i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, strip.Color(0, 150, (int) (percentage / 100 * 255)));
+    }
+    strip.show();
+    delay(100);
+    strip.clear();
+  }
+}
