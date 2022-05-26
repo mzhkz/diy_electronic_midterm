11a7149a9e336fcd4bbe6776ea291a1145efb4d9
moz <moz@sfc.wide.ad.jp>
2022-05-26 17:26:57 +0900

    add* complete!

--- backend/main.py
+++ backend/main.py
@@ -3,20 +3,22 @@ import serial
 import time
 
 stacks = []
-STACK_SIZE = 5
-SERIAL_CODE = "/dev/cu.usbmodem1432101"
+STACK_SIZE = 7
+SERIAL_CODE = "COM3"
 
 previous_value = 0
 previous_cpu_usage = 0
 is_increasing = 123
 
-ser = serial.Serial(SERIAL_CODE, 115200, timeout = 0.5)
+ser = serial.Serial(SERIAL_CODE, 115200, timeout = 0.3)
 time.sleep(2)
 
 
 def get_single_value(previous_value, current_value):
+    if previous_value == 0:
+        previous_value = 0.1
     code = current_value < previous_value
-    margin = previous_value * 0.05
+    margin = previous_value * 0.07
     if  previous_value-margin <= current_value <= previous_value+1:
         return 0
     else:
@@ -25,7 +27,7 @@ def get_single_value(previous_value, current_value):
 def main():
     global stacks, STACK_SIZE, previous_value, is_increasing
     while True:
-        percent_cpu_usage = psutil.cpu_percent(0.1)
+        percent_cpu_usage = psutil.cpu_percent(1)
         if percent_cpu_usage <= 0.0:
             percent_cpu_usage = previous_value
         else:
@@ -41,11 +43,11 @@ def main():
         current_value = sum(stacks) / STACK_SIZE
         is_increasing = get_single_value(current_value, previous_value)
         previous_value = sum(stacks) / STACK_SIZE
-        print("signal: {0}, usage: {1}, adj_usage: {2}".format(
-            is_increasing, 
-            percent_cpu_usage,
-            current_value)
-        )
+        # print("signal: {0}, usage: {1}, adj_usage: {2}".format(
+        #     is_increasing, 
+        #     percent_cpu_usage,
+        #     current_value)
+        # )
         print(" received -> {0}".format(ser.readline().decode('utf-8')))
         ser.write("{}:{}".format(is_increasing, int(current_value)).encode("utf-8"))
 
--- strandtest/strandtest.ino
+++ strandtest/strandtest.ino
@@ -8,11 +8,11 @@
 #define LED_PIN 6
 #define LED_COUNT 4
 int tick = 0;
+int prev_arg = 0;
 int pointer = 0;
 int ticks = 0;
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
-
 
 void setup() {
   #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
@@ -21,12 +21,9 @@ void setup() {
 
   strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
   strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
+//  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
   Serial.begin(115200);     // シリアル通信を初期化する。通信速度は9600bps
-  Serial.readString();
 }
-
-
 
 void loop() {
   if (Serial.available() > 0){
@@ -34,23 +31,28 @@ void loop() {
     String data = Serial.readString();
     int arg = data.substring(0,1).toInt();
     int percentage = data.substring(2).toInt();
-    int red = (int) 255 - (percentage / 100 * 255);
-    int blue = (int) (percentage / 100 * 255);
+    int red = (int) (percentage / 100.0 * 255.0);
+    int blue = (int) 255 - (percentage / 100.0 * 255.0);
 
     if (arg == 1) {
       pointer +=1;
-//      Serial.println("+");
+      if (prev_arg == 0) {
+        pointer = 0;
+      }
     } else if(arg == 2) {
       pointer -=1;
-//      Serial.println("-");
+      if (prev_arg == 0) {
+        pointer = LED_COUNT-1;
+      }
     } else if (arg == 0) {
       pointer = -2;
-//      Serial.println("=");
     }
+
+    prev_arg = arg;
 
     if (pointer == -2) {
       for (int i = 0; i < LED_COUNT; i++) {
-        strip.setPixelColor(i, strip.Color(red, 150, blue));
+        strip.setPixelColor(i, strip.Color(red, blue, 0));
       }
     } else {
       if (pointer < 0) {
@@ -58,9 +60,9 @@ void loop() {
       } else if (pointer >= LED_COUNT) {
         pointer = 0;
       }
-      strip.setPixelColor(pointer, strip.Color(red, 150, blue));
+      strip.setPixelColor(pointer, strip.Color(red, blue, 0));
     }
-    Serial.println(pointer);
+    Serial.println(arg);
     strip.show();
   }
 }
